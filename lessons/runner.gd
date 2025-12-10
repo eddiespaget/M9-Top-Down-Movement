a4709ce46658c404b41e58dcefb80f6ded10448d
eddiespaget <edsalas@s.sfusd.edu>
2025-12-10 13:44:03 -0800

    M9L7 done

--- lessons/runner.gd
+++ lessons/runner.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 @export var max_speed := 600.0
 @export var acceleration := 1200.0
 @export var deceleration := 1080.0
+
 @onready var _runner_visual: RunnerVisual = %RunnerVisualRed
+@onready var _dust: GPUParticles2D = %Dust
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -13,14 +15,19 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+		
 	move_and_slide()
+	
 	if direction.length() > 0.0:
 		_runner_visual.angle = rotate_toward(_runner_visual.angle, direction.orthogonal().angle(), 8.0 * delta)
+
 		var current_speed_percent := velocity.length() / max_speed
 		_runner_visual.animation_name = (
 			RunnerVisual.Animations.WALK
 			if current_speed_percent < 0.8
 			else RunnerVisual.Animations.RUN
 		)
+		_dust.emitting = true
 	else:
 		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
+		_dust.emitting = false
