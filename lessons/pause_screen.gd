f5eff08fac3253673586659f381ffdec9a7ea1e3
eddiespaget <edsalas@s.sfusd.edu>
2025-12-12 15:48:02 -0800

    M9L11 making the pause screen interactive
    continue at home maybe

--- lessons/pause_screen.gd
+++ lessons/pause_screen.gd
@@ -1,6 +1,11 @@
 @tool
 extends Control
 
+@export_range(0.1, 10.0, 0.01, "or_greater") var animation_duration := 2.3
+
+var _tween: Tween
+var _is_currently_opening := false
+
 @onready var _blur_color_rect: ColorRect = %BlurColorRect
 @onready var _ui_panel_container: PanelContainer = %UIPanelContainer
 
@@ -17,3 +22,20 @@ func set_menu_opened_amount(amount: float) -> void:
 	_blur_color_rect.material.set_shader_parameter("blur_amount", lerp(0.0, 1.5, amount))
 	_blur_color_rect.material.set_shader_parameter("tint_strength", lerp(0.0, 0.2, amount))
 	_ui_panel_container.modulate.a = amount
+
+func toggle() -> void:
+	_is_currently_opening = not _is_currently_opening
+
+	var duration := animation_duration
+
+	if _tween != null:
+		if not _is_currently_opening:
+			duration = _tween.get_total_elapsed_time()
+		_tween.kill()
+
+	_tween = create_tween()
+	_tween.set_ease(Tween.EASE_OUT)
+	_tween.set_trans(Tween.TRANS_QUART)
+
+	var target_amount := 1.0 if _is_currently_opening else 0.0
+	_tween.tween_property(self, "menu_opened_amount", target_amount, duration)
